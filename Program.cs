9b2368f2802e8026d838ba748570252211fc95f8
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-12 17:49:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 7;
+            while(number < 100)
+            {
+                Console.Write(number + " ");
+                number += 7;
+            }
+        }
+    }
+}
