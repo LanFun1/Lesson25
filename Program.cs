ddc7e0ddb082bcb045508258fc26fda330dc5482
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-13 16:22:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 7; i < 100; i+=7)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
